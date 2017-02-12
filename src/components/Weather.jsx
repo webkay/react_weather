8edd3327bda5c78f1e0cd3b4973e56bea4c6053f
kangkyu <websitekay@gmail.com>
2017-02-12 00:59:32 -0800

    make API call with axios

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,14 +1,33 @@
 var React = require('react')
 var WeatherForm = require('./WeatherForm')
 var WeatherMessage = require('./WeatherMessage')
+var openWeatherMap = require('../api/openWeatherMap')
 
 var Weather = React.createClass({
+  getInitialState: function () {
+    return {
+      location: 'Miami',
+      temp: 88
+    }
+  },
+  handleSearch: function (location) {
+    var that = this;
+    openWeatherMap.getTemp(location).then(function (temp) {
+      that.setState({
+        location: location,
+        temp: temp
+      })
+    }, function (errorMessage) {
+      alert(errorMessage);
+    });
+  },
   render: function () {
+    var {temp, location} = this.state;
     return (
       <div>
         <h3>Get Weather</h3>
-        <WeatherForm/>
-        <WeatherMessage/>
+        <WeatherForm onSearch={this.handleSearch}/>
+        <WeatherMessage temp={temp} location={location}/>
       </div>
     );
   }
--- src/components/WeatherMessage.jsx
+++ src/components/WeatherMessage.jsx
@@ -1,9 +1,9 @@
 var React = require('react')
-
 var WeatherMessage = React.createClass({
   render: function () {
+    var {temp, location} = this.props;
     return (
-      <h3>It's 40 in Philadelphia.</h3>
+      <h3>It's {temp} in {location}.</h3>
     )
   }
 });
