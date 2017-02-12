88f7d6b76b0a4356372ce69f599793d189a8dafc
websitekay <websitekay@gmail.com>
2017-02-12 03:55:55 -0800

    style Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,17 @@ var {Link, IndexLink} = require('react-router');
 
 var Nav = () => {
   return (
-    <div>
-      <h2>Nav component</h2>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Weather</li>
+          <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+          <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+          <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+      </div>
     </div>
   );
 };
