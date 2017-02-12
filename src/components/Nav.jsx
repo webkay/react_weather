e87b5edcb0cf80e7d29b0c6a77b8db9a6d060b66
kangkyu <websitekay@gmail.com>
2017-02-11 22:11:40 -0800

    Add two pages About and Examples

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+var React = require('react')
+var {Link} = require('react-router')
+
+var Nav = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h2>Nav component</h2>
+        <Link to="/">Get Weather</Link>
+        <Link to="/about">About</Link>
+        <Link to="/examples">Examples</Link>
+      </div>
+    );
+  }
+});
+
+module.exports = Nav;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import './index.css';
 var {Route, Router, IndexRoute, hashHistory} = require('react-router')
 // var Route = require('react-router').Route;
 
-var Main = require('./Main')
+var Main = require('./components/Main')
+var Weather = require('./components/Weather')
+var About = require('./components/About')
+var Examples = require('./components/Examples')
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
+      <Route path="/about" component={About}/>
+      <Route path="/examples" component={Examples}/>
+      <IndexRoute component={Weather}/>
     </Route>
   </Router>,
   document.getElementById('root')
